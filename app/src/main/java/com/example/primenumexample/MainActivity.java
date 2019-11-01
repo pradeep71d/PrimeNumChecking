ab443255dacb739881282b51765ef2bf45c76c9e
sandeep <sandeep@ashmar.in>
2019-11-01 16:23:48 +0530

    add

--- app/src/main/java/com/example/primenumexample/MainActivity.java
+++ app/src/main/java/com/example/primenumexample/MainActivity.java
@@ -11,19 +11,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int i,m=0,flag=0;
-        int n=3;//it is the number to be checked
+        int n=20;//it is the number to be checked
         m=n/2;
         if(n==0||n==1){
             System.out.println(n+" is not prime number");
         }else{
-            for(i=2;i<=m;i++){
+            for(i=2;i<=m;i++){//m=9
                 if(n%i==0){
                     System.out.println(n+" is not prime number");
-                    flag=1;
+               flag=1;
+
                     break;
                 }
             }
-            if(flag==0)  { System.out.println(n+" is prime number"); }
+            if(flag==0)  {
+                System.out.println(n+" is prime number");
+            }
         }//end of else
     }
 }
